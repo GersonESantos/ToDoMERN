5cca874ca2a831a04de1ed852a1a27d75fb1c38d
GersonESS <gebhsantos@gmail.com>
2026-01-06 18:31:09 -0300

    a

---
+++ 79/client/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const Login = () => {
+    return(
+        <div>
+        <h1>Task</h1>
+      
+        <p className="read-the-docs">
+        login
+      </p>
+        </div>
+        )
+};
+
+export default Login;
+
+//
