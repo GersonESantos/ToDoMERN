516b55e315eb6e08ec7a68f33cfb85571a28a6b4
GersonESS <gebhsantos@gmail.com>
2026-01-06 17:18:24 -0300

    a

---
+++ 77/client/todo/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const Login = () => {
+    return(
+        <div>
+        <h1>Vite + React</h1>
+      
+        <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+        </div>
+        )
+};
+
+export default Login;
+
+//
