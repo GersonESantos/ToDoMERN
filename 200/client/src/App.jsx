9909fa60083a807ed9409c35d06362be3f306c8e
GersonESS <gebhsantos@gmail.com>
2026-01-08 00:00:00 -0300

    a

---
+++ 200/client/src/App.jsx
@@ -0,0 +1,63 @@
+// import './App.css'
+import { Routes, Route, Navigate, Outlet, useLocation, useSelector} from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import TaskDetails from './pages/TaskDetails.jsx'
+import Tasks from './pages/Tasks.jsx'
+import Users from './pages/Users.jsx'
+import Trash from './pages/Trash.jsx'
+import Dashbord from './pages/Dashboard.jsx'
+
+function Layout() {
+  const { user } = useSelector((state) => state.auth);
+  const location = useLocation();
+
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-white dark:bg-[#1f1f1f] sticky top-0 hidden md:block'>
+        <Sidebar />
+      </div>
+
+      <MobileSidebar />
+
+      <div className='flex-1 overflow-y-auto'>
+        <Navbar />
+
+        <div className='p-4 2xl:px-10'>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to='/log-in' state={{ from: location }} replace />
+  );
+}
+
+
+
+
+
+function App() {
+  return (
+   <main className='w-full min-h-screen  bg-[#f3f4f6]'>
+            <Routes>
+                <Route element={<Layout />}>
+                  <Route index path="/" element={<Navigate to="/dashboard" />} />
+                    <Route path='/dashboard >'  element={/Dashbord /} />
+                    <Route path='/task >'  element={/Task /} />
+                    <Route path='/completed/:status >'  element={/Task /} />
+                    <Route path='/in-progress/:status >'  element={/Task /} />
+                    <Route path='/todo/:status >'  element={/Task /} />
+                    <Route path='/team >'  element={/Users /} />
+                    <Route path='/trash >'  element={/Trash /} />
+                    <Route path='/task/:id >'  element={/TaskDetails /} />                    
+                </Route>
+
+                <Route path='/log-in' element={<Login />} />
+            </Routes>
+
+            
+        </main>
+  );
+};
+
+export default App;
