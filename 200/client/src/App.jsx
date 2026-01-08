199c3f8f439e2f9cf1edcc626952872ff2b3a010
GersonESS <gebhsantos@gmail.com>
2026-01-08 01:23:29 -0300

    Update App.jsx

--- 200/client/src/App.jsx
+++ 200/client/src/App.jsx
@@ -1,5 +1,5 @@
 // import './App.css'
-import { Routes, Route, Navigate, Outlet, useLocation, useSelector} from 'react-router-dom'
+import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import TaskDetails from './pages/TaskDetails.jsx'
 import Tasks from './pages/Tasks.jsx'
@@ -8,9 +8,8 @@ import Trash from './pages/Trash.jsx'
 import Dashbord from './pages/Dashboard.jsx'
 
 function Layout() {
-  const { user } = useSelector((state) => state.auth);
-  const location = useLocation();
-
+  const { user } = "";
+   const location = useLocation();
   return user ? (
     <div className='w-full h-screen flex flex-col md:flex-row'>
       <div className='w-1/5 h-screen bg-white dark:bg-[#1f1f1f] sticky top-0 hidden md:block'>
@@ -38,25 +37,13 @@ function Layout() {
 
 function App() {
   return (
-   <main className='w-full min-h-screen  bg-[#f3f4f6]'>
-            <Routes>
-                <Route element={<Layout />}>
-                  <Route index path="/" element={<Navigate to="/dashboard" />} />
-                    <Route path='/dashboard >'  element={/Dashbord /} />
-                    <Route path='/task >'  element={/Task /} />
-                    <Route path='/completed/:status >'  element={/Task /} />
-                    <Route path='/in-progress/:status >'  element={/Task /} />
-                    <Route path='/todo/:status >'  element={/Task /} />
-                    <Route path='/team >'  element={/Users /} />
-                    <Route path='/trash >'  element={/Trash /} />
-                    <Route path='/task/:id >'  element={/TaskDetails /} />                    
-                </Route>
-
-                <Route path='/log-in' element={<Login />} />
-            </Routes>
-
-            
-        </main>
+   <main className='w-full min-h-screen  bg-[#f3f4f6]'>     
+      
+       
+            <Login />     
+      
+      
+      </main>
   );
 };
 
