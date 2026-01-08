c879e677547aebaa5ba1340eb20b27460297dd6e
GersonESS <gebhsantos@gmail.com>
2026-01-08 00:20:08 -0300

    a

--- 200/client/src/pages/Login.jsx
+++ 200/client/src/pages/Login.jsx
@@ -3,19 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 const Login = () => {
-    const  user = ""
-    const {
-        register, 
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
-
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        user && navigate("/dashboard");
-    }, [user]);
-
     return(
         <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
             <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
