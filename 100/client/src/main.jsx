cc579ff41f6b37880602005a81e0371d8634ac38
GersonESS <gebhsantos@gmail.com>
2026-01-07 21:26:23 -0300

    d

--- 100/client/src/main.jsx
+++ 100/client/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
