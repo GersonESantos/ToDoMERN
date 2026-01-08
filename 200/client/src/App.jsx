a4a01832d4b38b1d24db05433e76e1fb5c5ee630
GersonESS <gebhsantos@gmail.com>
2026-01-08 02:11:26 -0300

    Update App.jsx

--- 200/client/src/App.jsx
+++ 200/client/src/App.jsx
@@ -31,20 +31,28 @@ function Layout() {
   );
 }
 
-
-
-
-
 function App() {
   return (
-   <main className='w-full min-h-screen  bg-[#f3f4f6]'>     
-      
-       
-            <Login />     
-      
-      
-      </main>
+   <main className='w-full min-h-screen  bg-[#f3f4f6]'>
+            <Routes>
+                <Route element={<Layout />}>
+                  <Route index path="/" element={<Navigate to="/dashboard" />} />
+                    <Route path='/dashboard'  element={<Dashbord />} />
+                    <Route path='/task'  element={<Tasks />} />
+                    <Route path='/completed/:status'  element={<Tasks />} />
+                    <Route path='/in-progress/:status'  element={<Tasks />} />
+                    <Route path='/todo/:status'  element={<Tasks />} />
+                    <Route path='/team'  element={<Users />} />
+                    <Route path='/trash'  element={<Trash />} />
+                    <Route path='/task/:id'  element={<TaskDetails />} />                    
+                </Route>
+
+                <Route path='/log-in' element={<Login />} />
+            </Routes>
+
+            
+        </main>
   );
-};
+}
 
 export default App;
