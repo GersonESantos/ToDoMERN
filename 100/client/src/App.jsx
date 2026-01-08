58224be7fd7914558e417738d32d102e5ea91ade
GersonESS <gebhsantos@gmail.com>
2026-01-07 23:40:11 -0300

    x

--- 100/client/src/App.jsx
+++ 100/client/src/App.jsx
@@ -1,11 +1,36 @@
 // import './App.css'
+import { Routes, Route, Navigate, Outlet, useLocation, useSelector} from 'react-router-dom'
 import Login from './pages/Login.jsx'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Dashbord from './pages/Dashboard.jsx'
+import TaskDetails from './pages/TaskDetails.jsx'
 import Tasks from './pages/Tasks.jsx'
 import Users from './pages/Users.jsx'
 import Trash from './pages/Trash.jsx'
-import TaskDetails from './pages/TaskDetails.jsx'
+import Dashbord from './pages/Dashboard.jsx'
+
+function Layout() {
+  const { user } = useSelector((state) => state.auth);
+  const location = useLocation();
+
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-white dark:bg-[#1f1f1f] sticky top-0 hidden md:block'>
+        <Sidebar />
+      </div>
+
+      <MobileSidebar />
+
+      <div className='flex-1 overflow-y-auto'>
+        <Navbar />
+
+        <div className='p-4 2xl:px-10'>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to='/log-in' state={{ from: location }} replace />
+  );
+}
 
 
 
@@ -30,7 +55,7 @@ function App() {
                 <Route path='/log-in' element={<Login />} />
             </Routes>
 
-            <Toaster richColors position='top-center' />
+            
         </main>
   )
 }
--- 100/client/src/pages/Login.jsx
+++ 100/client/src/pages/Login.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {useForm} from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+    const user = ""
+    const {
+        register, 
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        user && navigate("/dashboard");
+    }, [user, navigate]);
     return(
         <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
             <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
