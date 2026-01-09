27b8578e434858a3255c2d46b10cf1aabea40d71
GersonESS <gebhsantos@gmail.com>
2026-01-08 23:33:54 -0300

    a

---
+++ 208/client/src/App.jsx
@@ -0,0 +1,58 @@
+// import './App.css'
+import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import TaskDetails from './pages/TaskDetails.jsx'
+import Tasks from './pages/Tasks.jsx'
+import Users from './pages/Users.jsx'
+import Trash from './pages/Trash.jsx'
+import Dashbord from './pages/Dashboard.jsx'
+
+function Layout() {
+  const { user } = "";
+   const location = useLocation();
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-white dark:bg-[#1f1f1f] sticky top-0 hidden md:block'>
+        <Sidebar />
+      </div>
+
+      <MobileSidebar />
+
+      <div className='flex-1 overflow-y-auto'>
+        <Navbar />
+
+        <div className='p-4 2xl:px-10'>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to='/log-in' state={{ from: location }} replace />
+  );
+}
+
+const App = () => {
+  return (
+   <main className='w-full min-h-screen  bg-[#f3f4f6]'>
+            <Routes>
+                <Route element={<Layout />}>
+                  <Route index path="/" element={<Navigate to="/dashboard" />} />
+                    <Route path='/dashboard'  element={<Dashbord />} />
+                    <Route path='/task'  element={<Tasks />} />
+                    <Route path='/completed/:status'  element={<Tasks />} />
+                    <Route path='/in-progress/:status'  element={<Tasks />} />
+                    <Route path='/todo/:status'  element={<Tasks />} />
+                    <Route path='/team'  element={<Users />} />
+                    <Route path='/trash'  element={<Trash />} />
+                    <Route path='/task/:id'  element={<TaskDetails />} />                    
+                </Route>
+
+                <Route path='/log-in' element={<Login />} />
+            </Routes>
+
+            
+        </main>
+  );
+};
+
+export default App;
