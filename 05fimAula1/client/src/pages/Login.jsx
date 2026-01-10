0a3f21e86c8d8616fa7a8c545144212d2f148804
GersonESS <gebhsantos@gmail.com>
2026-01-10 18:08:48 -0300

    a

---
+++ 05fimAula1/client/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="p-4 space-y-6">
+      <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
+      <pre
+        className="bg-gray-100 p-4 rounded-lg text-lg font-mono overflow-auto">
+      </pre>
+    </div>
+  );
+};
+
+export default Login;
