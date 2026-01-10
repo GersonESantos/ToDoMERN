d35153164986fdbc9e5ce1bc86077757fd3a996e
GersonESS <gebhsantos@gmail.com>
2026-01-10 14:50:18 -0300

    a

---
+++ 00/client/postcss.config.js
@@ -0,0 +1,14 @@
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+// export default {
+//   plugins: {
+//     "postcss-import": {},
+//     tailwindcss: {},
+//     autoprefixer: {},
+//     "@tailwindcss/postcss": {},
+//   },
+// };
--- 00/client/src/pages/Login.jsx
+++ 00/client/src/pages/Login.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
   return (
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+    <div className="p-4 space-y-6">
+      <h1 className="text-3xl text-color-red font-bold underline">Hello world!</h1>
+      <pre className="bg-gray-100 p-4 rounded-lg text-lg font-mono overflow-auto">
+        
+      </pre>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
