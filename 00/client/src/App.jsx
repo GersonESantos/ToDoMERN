5d6a880269bbe22159022319219fa7462edab703
GersonESS <gebhsantos@gmail.com>
2026-01-11 16:41:17 -0300

    a

--- 00/client/src/App.jsx
+++ 00/client/src/App.jsx
@@ -7,10 +7,11 @@ import Trash from './pages/Trash.jsx'
 import Dashboard from "./pages/Dashboard.jsx"
 import { Toaster } from "sonner";
 import { Sidebar, MobileSidebar, Navbar } from "./components";
+import { useSelector } from "react-redux";
 
 
 function Layout() {
-  const { user } = "";
+  const { user } = useSelector((state) => state.auth);
   const location = useLocation();
   return user ? (
     <div className='w-full h-screen flex flex-col md:flex-row'>
