b111443e758a3a6893274453180503c9e1316f4d
GersonESS <gebhsantos@gmail.com>
2026-01-12 00:35:25 -0300

    s

---
+++ UserLoginApp/frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import UserList from './pages/UserList';
+import EditUser from './pages/EditUser';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/edit/:id" element={<EditUser />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- headlessu/00/client/src/App.jsx
+++ headlessu/00/client/src/App.jsx
@@ -2,10 +2,11 @@ import Menu from "./pages/menu.jsx";
 import Switch from "./pages/Switch.jsx";
 function App() {
   return (
-    <div>
+    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", height: "100vh", padding: "0 20px" }}>
       <Menu />
-
-      <Switch />
+      <div style={{ marginLeft: "auto" }}>
+        <Switch />
+      </div>
     </div>
   );
 }
