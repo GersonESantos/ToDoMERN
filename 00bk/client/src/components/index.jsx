2b98a134ffe32cd8b603c144f84e7646a5bbcca6
GersonESS <gebhsantos@gmail.com>
2026-01-12 14:42:19 -0300

    d

--- 00bk/client/src/components/index.jsx
+++ 00bk/client/src/components/index.jsx
@@ -4,5 +4,6 @@ import Textbox from "./Textbox";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import MobileSidebar from "./MobileSidebar";
+import ModalWrapper from "./ModalWrapper";
 
-export { Button, Loading, Textbox, Sidebar, Navbar, MobileSidebar };
+export { Button, Loading, Textbox, Sidebar, Navbar, MobileSidebar, ModalWrapper };
