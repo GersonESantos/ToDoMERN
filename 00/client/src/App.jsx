1cc762c82ca5e38366c60e30a244ac9a037392c3
GersonESS <gebhsantos@gmail.com>
2026-01-12 19:28:16 -0300

    Update App.jsx

--- 00/client/src/App.jsx
+++ 00/client/src/App.jsx
@@ -8,7 +8,7 @@ import { Toaster } from "sonner";
 import { Navbar, Sidebar } from "./components";
 import Users from "./pages/Users";
 import Dashboard from "./pages/Dashboard";
-import Login  from "./pages/Login";
+import Login from "./pages/Login";
 import Tasks from "./pages/Tasks";
 import TaskDetail from "./pages/TaskDetails";
 import Trash from "./pages/Trash";
@@ -19,23 +19,23 @@ function Layout() {
   const location = useLocation();
 
   return user ? (
-    <div className='w-full h-screen flex flex-col md:flex-row'>
-      <div className='w-1/5 h-screen bg-white dark:bg-[#1f1f1f] sticky top-0 hidden md:block'>
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white dark:bg-[#1f1f1f] sticky top-0 hidden md:block">
         <Sidebar />
       </div>
 
       <MobileSidebar />
 
-      <div className='flex-1 overflow-y-auto'>
+      <div className="flex-1 overflow-y-auto">
         <Navbar />
 
-        <div className='p-4 2xl:px-10'>
+        <div className="p-4 2xl:px-10">
           <Outlet />
         </div>
       </div>
     </div>
   ) : (
-    <Navigate to='/log-in' state={{ from: location }} replace />
+    <Navigate to="/log-in" state={{ from: location }} replace />
   );
 }
 
@@ -46,72 +46,72 @@ const MobileSidebar = () => {
 
   const closeSidebar = () => {
     dispatch(setOpenSidebar(false));
-  }; 
+  };
 
   return (
-      <>
-            <Transition
-              show={isSidebarOpen}
-              as={Fragment}
-              enter='transition-opacity duration-700'
-              enterFrom='opacity-x-10'
-              enterTo='opacity-x-100'
-              leave='transition-opacity duration-700'
-              leaveFrom='opacity-x-100'
-              leaveTo='opacity-x-0'
-            >
-              {(ref) => (
-                <div
-                  ref={(node) => (mobileMenuRef.current = node)}
-                  className={`md:hidden w-full h-full bg-black/40 transition-transform duration-700 transform
+    <>
+      <Transition
+        show={isSidebarOpen}
+        as={Fragment}
+        enter="transition-opacity duration-700"
+        enterFrom="opacity-x-10"
+        enterTo="opacity-x-100"
+        leave="transition-opacity duration-700"
+        leaveFrom="opacity-x-100"
+        leaveTo="opacity-x-0"
+      >
+        {(ref) => (
+          <div
+            ref={(node) => (mobileMenuRef.current = node)}
+            className={`md:hidden w-full h-full bg-black/40 transition-transform duration-700 transform
                    ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}
+            onClick={() => closeSidebar()}
+          >
+            <div className="bg-white w-3/4 h-full">
+              <div className="w-full flex justify-end px-5 pt-5">
+                <button
                   onClick={() => closeSidebar()}
+                  className="flex justify-end items-end"
                 >
-                  <div className='bg-white w-3/4 h-full'>
-                    <div className='w-full flex justify-end px-5 pt-5'>
-                      <button
-                        onClick={() => closeSidebar()}
-                        className='flex justify-end items-end'
-                      >
-                        <IoMdClose size={25} />
-                      </button>
-                    </div>
-      
-                    <div className='-mt-10'>
-                      <Sidebar />
-                    </div>
-                  </div>
-                </div>
-              )}
-            </Transition>
-          </>
-        );
-      };
-      
-      const App = () => {
-        const theme = "light";
-      
-        return (
-          <main className={theme}>
-            <div className='w-full min-h-screen bg-[#f3f4f6] dark:bg-[#0d0d0df4]'>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index psth='/' element={<Navigate to='/dashboard' />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/tasks' element={<Tasks />} />
-          <Route path='/completed/:status?' element={<Tasks />} />
-          <Route path='/in-progress/:status?' element={<Tasks />} />
-          <Route path='/todo/:status?' element={<Tasks />} />
-          <Route path='/trashed' element={<Trash />} />
-          <Route path='/task/:id' element={<TaskDetail />} />
-          <Route path='/team' element={<Users />} />
-        </Route>
+                  <IoMdClose size={25} />
+                </button>
+              </div>
 
-        <Route path='/log-in' element={<Login />} />
-      </Routes>
+              <div className="-mt-10">
+                <Sidebar />
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </>
+  );
+};
+
+const App = () => {
+  const theme = "light";
+
+  return (
+    <main className={theme}>
+      <div className="w-full min-h-screen bg-[#f3f4f6] dark:bg-[#0d0d0df4]">
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index psth="/" element={<Navigate to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/completed/:status?" element={<Tasks />} />
+            <Route path="/in-progress/:status?" element={<Tasks />} />
+            <Route path="/todo/:status?" element={<Tasks />} />
+            <Route path="/trashed" element={<Trash />} />
+            <Route path="/task/:id" element={<TaskDetail />} />
+            <Route path="/team" element={<Users />} />
+          </Route>
+
+          <Route path="/log-in" element={<Login />} />
+        </Routes>
       </div>
 
-      <Toaster richColors position='top-center' />
+      <Toaster richColors position="top-center" />
     </main>
   );
 };
