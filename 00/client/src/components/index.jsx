792a15f0e088b1bf0620c01eda5ea5db5c65bd02
GersonESS <gebhsantos@gmail.com>
2026-01-12 15:05:04 -0300

    f

--- 00/client/src/components/index.jsx
+++ 00/client/src/components/index.jsx
@@ -1,9 +1,36 @@
+import AddUser from "./AddUser";
 import Button from "./Button";
+import ChangePassword from "./ChangePassword";
+import { Chart } from "./Chart";
+import ConfirmatioDialog, { UserAction } from "./ConfirmationDialog";
 import Loading from "./Loading";
-import Textbox from "./Textbox";
-import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
-import MobileSidebar from "./MobileSidebar";
 import ModalWrapper from "./ModalWrapper";
+import Navbar from "./Navbar";
+import SelectList from "./SelectList";
+import Sidebar from "./Sidebar";
+import Table from "./Table";
+import Tabs from "./Tabs";
+import Textbox from "./Textbox";
+import Title from "./Title";
+import UserAvatar from "./UserAvatar";
+import UserInfo from "./UserInfo";
 
-export { Button, Loading, Textbox, Sidebar, Navbar, MobileSidebar, ModalWrapper };
+export {
+  AddUser,
+  Button,
+  ChangePassword,
+  Chart,
+  ConfirmatioDialog,
+  Loading,
+  ModalWrapper,
+  Navbar,
+  SelectList,
+  Sidebar,
+  Table,
+  Tabs,
+  Textbox,
+  Title,
+  UserAction,
+  UserAvatar,
+  UserInfo,
+};
