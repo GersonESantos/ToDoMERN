19bffecfbe377bde235a8f2c358b80d2b4cce975
GersonESS <gebhsantos@gmail.com>
2026-01-12 18:37:44 -0300

    meio da aula3

--- 00bk/client/src/App.jsx
+++ 00bk/client/src/App.jsx
@@ -1,17 +1,23 @@
-import Login from "./pages/Login.jsx";
-import { Route, Routes, Navigate, Outlet, useLocation } from "react-router-dom";
-import TaskDetails from './pages/TaskDetails.jsx'
-import Tasks from './pages/Tasks.jsx'
-import Users from './pages/Users.jsx'
-import Trash from './pages/Trash.jsx'
-import Dashboard from "./pages/Dashboard.jsx"
+//00/client/src/App.jsx
+import { Transition } from "@headlessui/react";
+import { Fragment, useRef } from "react";
+import { IoMdClose } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
-import { Sidebar, MobileSidebar, Navbar } from "./components";
-import { useSelector } from "react-redux";
+import { Navbar, Sidebar } from "./components";
+import Users from "./pages/Users";
+import Dashboard from "./pages/Dashboard";
+import Login  from "./pages/Login";
+import Tasks from "./pages/Tasks";
+import TaskDetail from "./pages/TaskDetails";
+import Trash from "./pages/Trash";
+import { setOpenSidebar } from "./redux/slices/authSlice";
 
 function Layout() {
   const { user } = useSelector((state) => state.auth);
   const location = useLocation();
+
   return user ? (
     <div className='w-full h-screen flex flex-col md:flex-row'>
       <div className='w-1/5 h-screen bg-white dark:bg-[#1f1f1f] sticky top-0 hidden md:block'>
@@ -33,28 +39,81 @@ function Layout() {
   );
 }
 
+const MobileSidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => state.auth);
+  const mobileMenuRef = useRef(null);
+  const dispatch = useDispatch();
 
+  const closeSidebar = () => {
+    dispatch(setOpenSidebar(false));
+  }; 
 
-function App() {
   return (
-    <main className="w-full min-h-screen bg-white-500">
+      <>
+            <Transition
+              show={isSidebarOpen}
+              as={Fragment}
+              enter='transition-opacity duration-700'
+              enterFrom='opacity-x-10'
+              enterTo='opacity-x-100'
+              leave='transition-opacity duration-700'
+              leaveFrom='opacity-x-100'
+              leaveTo='opacity-x-0'
+            >
+              {(ref) => (
+                <div
+                  ref={(node) => (mobileMenuRef.current = node)}
+                  className={`md:hidden w-full h-full bg-black/40 transition-transform duration-700 transform
+                   ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}
+                  onClick={() => closeSidebar()}
+                >
+                  <div className='bg-white w-3/4 h-full'>
+                    <div className='w-full flex justify-end px-5 pt-5'>
+                      <button
+                        onClick={() => closeSidebar()}
+                        className='flex justify-end items-end'
+                      >
+                        <IoMdClose size={25} />
+                      </button>
+                    </div>
+      
+                    <div className='-mt-10'>
+                      <Sidebar />
+                    </div>
+                  </div>
+                </div>
+              )}
+            </Transition>
+          </>
+        );
+      };
+      
+      const App = () => {
+        const theme = "light";
+      
+        return (
+          <main className={theme}>
+            <div className='w-full min-h-screen bg-[#f3f4f6] dark:bg-[#0d0d0df4]'>
       <Routes>
         <Route element={<Layout />}>
-          <Route index path="/" element={<Navigate to="/dashboard" />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/task" element={<Tasks />} />
-          <Route path="/completed/:status" element={<Tasks />} />
-          <Route path="/in-progress/:status" element={<Tasks />} />
-          <Route path="/todo/:status" element={<Tasks />} />
-          <Route path="/team" element={<Users />} />
-          <Route path="/trash" element={<Trash />} />
-          <Route path="/task/:id" element={<TaskDetails />} />
+          <Route index psth='/' element={<Navigate to='/dashboard' />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='/completed/:status?' element={<Tasks />} />
+          <Route path='/in-progress/:status?' element={<Tasks />} />
+          <Route path='/todo/:status?' element={<Tasks />} />
+          <Route path='/trashed' element={<Trash />} />
+          <Route path='/task/:id' element={<TaskDetail />} />
+          <Route path='/team' element={<Users />} />
         </Route>
+
         <Route path='/log-in' element={<Login />} />
       </Routes>
-      <Toaster richColors></Toaster>
+      </div>
+
+      <Toaster richColors position='top-center' />
     </main>
   );
-}
+};
 
 export default App;
