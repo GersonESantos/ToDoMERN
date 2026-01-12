7c053088c0fa93b02679d31309afbe6d3d1a4bf6
GersonESS <gebhsantos@gmail.com>
2026-01-12 13:07:52 -0300

    a

--- 00bk/client/src/components/index.jsx
+++
@@ -1,8 +0,0 @@
-import Button from "./Button";
-import Loading from "./Loading";
-import Textbox from "./Textbox";
-import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
-import MobileSidebar from "./MobileSidebar";
-
-export { Button, Loading, Textbox, Sidebar, Navbar, MobileSidebar };
