d532153532bd3922e401614c0a21c449f3089186
GersonESS <gebhsantos@gmail.com>
2026-01-14 20:58:00 -0300

    a

---
+++ 09bancoConect/client/src/components/MobileSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MobileSidebar = () => {
+    return (
+        <div className=''>
+            MobileSidebar
+        </div>
+    );
+};
+
+export default MobileSidebar;
