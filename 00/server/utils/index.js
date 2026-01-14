79b70c6a7b7e46eacef1473414ddaec3d687f8ce
GersonESS <gebhsantos@gmail.com>
2026-01-14 20:52:32 -0300

    Update index.js

--- 00/server/utils/index.js
+++ 00/server/utils/index.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 const dbConnection = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Database connected successfully");
-    } catch (error) {
-        console.error("Database connection error:", error);
-        throw new Error("Failed to connect to the database");
-    }
+try {
+   await mongoose.connect(process.env.MONGODB_URI)
+ console.log("MongoDB Atlas CONECTADO!")
+} catch (error) {
+    console.log("Db Error:" + error );
 };
+}
 
 export default dbConnection;
+
+
