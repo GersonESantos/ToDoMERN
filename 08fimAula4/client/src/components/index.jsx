74a775deb6ee7e4c69b2b757ff9b92393325a09d
GersonESS <gebhsantos@gmail.com>
2026-01-14 08:30:02 -0300

    t

---
+++ 08fimAula4/client/src/components/index.jsx
@@ -0,0 +1,11 @@
+import Button from "./Button";
+import Chart from "./Chart";
+import UserInfo from "./UserInfo";
+import Loading from "./Loading";
+import Textbox from "./Textbox";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+import MobileSidebar from "./MobileSidebar";
+import ModalWrapper from "./ModalWrapper";
+
+export { Button, Loading, Textbox, Sidebar, Navbar, MobileSidebar, ModalWrapper, Chart, UserInfo };
