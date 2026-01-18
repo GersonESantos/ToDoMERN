9a8d78761c785ac17ad8057e7bed96d88022b6d2
GersonESS <gebhsantos@gmail.com>
2026-01-18 14:25:11 -0300

    x

---
+++ ver2/00bk/client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+const Login = () => {
+  return (
+    <div className="p-4 space-y-6">
+      <h1 className="text-3xl text-red-500 font-bold underline">Consultorio</h1>
+      <pre
+        className="bg-gray-100 p-4 rounded-lg text-lg font-mono overflow-auto">
+      </pre>
+    </div>
+  );
+};
+
+export default Login;
