f33fb3b3d454418daec19d1d0bd7b9c17adc79f5
GersonESS <gebhsantos@gmail.com>
2026-01-19 17:04:13 -0300

    s

--- ver2/00/client/src/App.jsx
+++ ver2/00/client/src/App.jsx
@@ -2,7 +2,16 @@ import Login from './pages/Login.jsx'
 
 function App() {
   return (
-    <Login />
+    <main className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row ">
+      <Routes>
+        <Route  element={<Layout />} >
+          <Route path="/" element={<Navigator to="/dashboard" />} />
+           
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        
+      </Routes>
+    </main>
   )
 }
 
--- ver2/00/client/src/pages/Login.jsx
+++ ver2/00/client/src/pages/Login.jsx
@@ -1,10 +1,9 @@
+import React from "react";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 const Login = () => {
   return (
-    <div className="p-4 space-y-6">
-      <h1 className="text-3xl text-red-500 font-bold underline">Consultorio</h1>
-      <pre
-        className="bg-gray-100 p-4 rounded-lg text-lg font-mono overflow-auto">
-      </pre>
+    <div>
+      <h1>Login Page</h1>
     </div>
   );
 };
