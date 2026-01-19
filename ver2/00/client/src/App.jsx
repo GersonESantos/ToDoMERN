40a3539682acc5e87137e7c4e019309e538061d6
GersonESS <gebhsantos@gmail.com>
2026-01-19 18:04:10 -0300

    v

--- ver2/00/client/src/App.jsx
+++ ver2/00/client/src/App.jsx
@@ -3,14 +3,12 @@ import Login from './pages/Login.jsx'
 
 function App() {
   return (
-    <Router>
-      <main className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row ">
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </main>
-    </Router>
+    <main className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row ">
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </main>
   )
 }
 
