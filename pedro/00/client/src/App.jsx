5b8c8edea9923ffcbeff03444000a583f54ef486
GersonESS <gebhsantos@gmail.com>
2026-02-05 16:23:02 -0300

    s

--- pedro/00/client/src/App.jsx
+++ pedro/00/client/src/App.jsx
@@ -1,5 +1,5 @@
 
-
+import Login from './pages/Login'
 
 function App() {
   
@@ -7,15 +7,7 @@ function App() {
   return (
     <>
       
-     <section>
-  <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-  <img class="size-12 shrink-0" src="../public/Gersonfb2025.png" alt="ChitChat Logo" />
-  <div>
-    <div class="text-xl font-medium text-black dark:text-white">Gerson Eustaquio Dos Santos</div>
-    <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
-  </div>
-</div>
-</section> 
+      <Login />
       
       
     </>
